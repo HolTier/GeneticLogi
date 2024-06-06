77f41492cdae26e878b1057e140e2d9f8dcbad56
Marcin Holcman <62081203+HolTier@users.noreply.github.com>
2024-06-06 18:08:26 +0200

    Backend initialization

---
+++ GeneticLogi-Backend/Program.cs
@@ -0,0 +1,15 @@
+namespace GeneticLogi_Backend
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
