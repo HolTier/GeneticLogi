7806edad2c576ba6606833b5989e6614e720c996
Marcin Holcman <62081203+HolTier@users.noreply.github.com>
2024-06-09 01:30:02 +0200

    Added a generic repository

---
+++ GeneticLogi-Backend/Data/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeneticLogi_Backend.Data
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly GeneticLogiDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(GeneticLogiDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+            _context.SaveChanges();
+        }
+    }
+}
